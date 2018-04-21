c323f5b0a9124012c05a85ec74f4c3a487067787
learn-with-abhi <smile_with_abhi@hotmail.com>
2018-04-21 21:57:19 +0530

    changed files for amount and date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseItem = ({id, description, amount, createdAt}) => (
         <tr>
             <td><Link to={`/edit/${id}`}>{description}</Link></td>
-            <td>{amount}</td>
-            <td>{createdAt}</td>
+            <td>{numeral(amount / 100).format('$0,0.00')}</td>
+            <td>{moment(createdAt).format('MMMM Do, YYYY')}</td>
         </tr>
 );
 
